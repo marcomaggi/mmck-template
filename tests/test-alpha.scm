0a6027e3b250e7baed134e7f5c2570d52de6b59e
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-04-02 08:34:24 +0200

    added test program

---
+++ tests/test-alpha.scm
@@ -0,0 +1,60 @@
+;;; -*- coding: utf-8-unix  -*-
+;;;
+;;;Part of: MMUX CHICKEN Template
+;;;Contents: test program for demo
+;;;Date: Apr  2, 2019
+;;;
+;;;Abstract
+;;;
+;;;	This program is a demo of the features.
+;;;
+;;;Copyright (C) 2019 Marco Maggi <marco.maggi-ipsu@poste.it>
+;;;
+;;;This program is free software: you can  redistribute it and/or modify it under the
+;;;terms of the GNU  Lesser General Public License as published  by the Free Software
+;;;Foundation,  either version  3  of the  License,  or (at  your  option) any  later
+;;;version.
+;;;
+;;;This program is  distributed in the hope  that it will be useful,  but WITHOUT ANY
+;;;WARRANTY; without  even the implied warranty  of MERCHANTABILITY or FITNESS  FOR A
+;;;PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+;;;
+;;;You should  have received a  copy of the GNU  Lesser General Public  License along
+;;;with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+
+
+;;;; units and module header
+
+(require-library (mmux template))
+(require-library (mmux checks))
+
+(module (test-demo)
+    ()
+  (import (scheme)
+	  (chicken base)
+	  (mmux template)
+	  (mmux checks))
+
+(check-set-mode! 'report-failed)
+(check-display "*** testing demo\n")
+
+
+;;;; stuff
+
+(parameterize ((check-test-name		'alpha))
+
+  (check
+      (the-func)
+    => #t)
+
+  (values))
+
+
+;;;; done
+
+(check-report)
+
+#| end of module |# )
+
+;;; end of file
